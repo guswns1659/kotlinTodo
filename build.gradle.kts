0816f60c7adc8c96a46930a8f92c6cdea444d53a
guswns1659 <guswns1659@gmail.com>
2021-02-20 16:08:20 +0900

    Test apache reflectionEquals()

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
     implementation("io.springfox:springfox-swagger2:2.9.2")
+    // apache common
+    implementation("org.apache.commons:commons-lang3:3.10")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
---
+++ src/main/kotlin/com/example/todo/test/Test.kt
@@ -0,0 +1,76 @@
+package com.example.todo.test
+
+import org.apache.commons.lang3.builder.EqualsBuilder
+
+fun main(args: Array<String>) {
+
+    val cardBill1 = CardBill().apply {
+        billId = "1"
+        billNumber = "123"
+        transaction = mutableListOf(
+            CardBillTransaction().apply {
+                transactionId = "1"
+                cardNumber = "456"
+            },
+            CardBillTransaction().apply {
+                transactionId = "2"
+                cardNumber = "789"
+            }
+        )
+    }
+
+    val cardBill2 = CardBill().apply {
+        billId = "1"
+        billNumber = "123"
+        transaction = mutableListOf(
+            CardBillTransaction().apply {
+                transactionId = "1"
+                cardNumber = "555"
+            }
+        )
+    }
+
+    val cardBill3 = CardBill().apply {
+        billId = "1"
+        billNumber = "123"
+        transaction = mutableListOf(
+            CardBillTransaction().apply {
+                transactionId = "1"
+                cardNumber = "456"
+            }
+        )
+    }
+
+    val cardBill4 = CardBill().apply {
+        billId = "1"
+        billNumber = "123"
+        transaction = mutableListOf(
+            CardBillTransaction().apply {
+                transactionId = "1"
+                cardNumber = "555"
+            }
+        )
+    }
+
+    val oldBills = mutableListOf<CardBill>()
+    oldBills.add(0, cardBill1)
+    oldBills.add(1, cardBill2)
+
+    val newBills = mutableListOf<CardBill>()
+    newBills.add(0, cardBill3)
+    newBills.add(0, cardBill4)
+
+    println("size result : ${oldBills.size == newBills.size}")
+    println("result : ${EqualsBuilder.reflectionEquals(cardBill1, cardBill2)}")
+}
+
+data class CardBill(
+    var billId: String? = null,
+    var billNumber: String? = null,
+    var transaction: MutableList<CardBillTransaction>? = null
+)
+
+data class CardBillTransaction(
+    var transactionId: String? = null,
+    var cardNumber: String? = null
+)
